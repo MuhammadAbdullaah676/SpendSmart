58edaa9c6a6cfe45f294045c1013144cd0f754c8
batool <tahiarb777@gmail.com>
2023-05-30 11:30:29 +0500

    dropdown

--- spend-smart/src/App.js
+++ spend-smart/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Budget from './components/Budget';
+import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
@@ -11,11 +11,11 @@ const App = () => {
 	return (
 		
 			<div className='container'>
-				<h1 className='mt-3'>My Budget Planner</h1>
+				<h1 className='mt-3'>Spend Smart</h1>
 				<div className='row mt-3'>
-					{/* <div className='col-sm'>
+					{<div className='col-sm'>
 						<Budget />
-					</div> */}
+					</div> }
 					<div className='col-sm'>
 						<Remaining />
 					</div>
--- spend-smart/src/components/AddExpenseForm.js
+++ spend-smart/src/components/AddExpenseForm.js
@@ -1,95 +1,95 @@
-
-import { Formik } from 'formik';
+import { Formik } from "formik";
 import * as yup from "yup";
 
-
-const expenseSchema =yup.object().shape({
-	expenseName:yup.string()
-	.min (3,"To short!")
-	.max (30,"To long!")
-	.required("required"),
-	expenseCost:yup.number()
-	.min(1,"to short!")
-	.max(100000,"To long!")
-	.required( "required")
-
-})
-
+const expenseSchema = yup.object().shape({
+  expenseName: yup
+    .string()
+    .min(3, "To short!")
+    .max(30, "To long!")
+    .required("required"),
+  expenseCost: yup
+    .number()
+    .min(1, "to short!")
+    .max(100000, "To long!")
+    .required("required"),
+});
 
 const AddExpenseForm = () => {
-	
-	return (
-		<Formik 
+  return (
+    <Formik
+      initialValues={{}}
+      validationSchema={expenseSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(props) => (
+        <form onSubmit={props.handleSubmit}>
+          <div>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="Dropdown" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a class="dropdown-item" href="action">
+                    Income
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="action">
+                    ExpenseName
+                  </a>
+                </li>
+                <li className="dropdown-divider"></li>
+              </ul>
+            </li>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <label htmlFor="name">Name</label>
+              <input
+                required="required"
+                type="text"
+                className="form-control"
+                id="name"
+                //required props
+                name="expenseName"
+                onChange={props.handleChange}
+                value={props.values.expenseName}
+              ></input>
+              {props.touched.expenseName && props.errors.expenseName ? (
+                <div style={{ color: "red" }}>{props.errors.expenseName}</div>
+              ) : null}
+            </div>
 
-		initialValues={{}}
-		validationSchema={expenseSchema }
-		onSubmit={(values) => {
-     
-			console.log(values)
-		}}
-		
-		
-		>
-		{(props)=>(	
-			<form onSubmit={props.handleSubmit}>
-			<div className='row'>
-		
-				<div className='col-sm'>
-					<label htmlFor='name'>Name</label>
-					<input
-
-						required='required'
-						type='text'
-						className='form-control'
-						id='name'
-						
-
-						//required props
-						name="expenseName"
-						onChange={props.handleChange}
-						value={props.values.expenseName}
-						
-						
-					
-					></input>
-					{props.touched.expenseName && props.errors.expenseName ? (
-                      <div style={{ color: "red"}}>{props.errors.expenseName}</div>
-                     ) : null}
-					 
-				</div>
-
-				<div className='col-sm'>
-					<label htmlFor='cost'>Cost</label>
-					<input
-						required='required'
-						type='text'
-						className='form-control'
-						id='cost'
-						 iu7465
-						name="expenseCost"
-						onChange={props.handleChange}
-						value={props.values.expenseCost}
-
-						
-					></input>
-g					{/* validation messegs */}
-					 {props.touched.expenseCost && props.errors.expenseCost ? (
-                      <div style={{ color: "red"}}> {props.errors.expenseCost}</div>
-                     ) : null}
-				</div>
-				<div className='col-sm'>
-					<button type='submit' className='btn btn-primary mt-3'>
-						Save
-					</button>
-				</div>
-			</div>
-		</form>
-
-		)}
-
-		
-		</Formik>
-	);
+            <div className="col-sm">
+              <label htmlFor="cost">Cost</label>
+              <input
+                required="required"
+                type="text"
+                className="form-control"
+                id="cost"
+                iu7465
+                name="expenseCost"
+                onChange={props.handleChange}
+                value={props.values.expenseCost}
+              ></input>
+              {/* validation messegs */}
+              {props.touched.expenseCost && props.errors.expenseCost ? (
+                <div style={{ color: "red" }}> {props.errors.expenseCost}</div>
+              ) : null}
+            </div>
+            <div className="col-sm">
+              <button type="submit" className="btn btn-primary mt-3">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
 };
 
 export default AddExpenseForm;
