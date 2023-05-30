7189966a641aa7b1ceffd4a6f9c28f50ea277e80
MuhammadAbdullah676 <abdullaah676@gmail.com>
2023-05-30 12:48:29 +0500

    feature: aboutus,navbar,and back-end(login-signup-expenses) added and merged successfully

--- spend-smart/src/App.js
+++ spend-smart/src/App.js
@@ -1,22 +1,33 @@
-import './App.css';
-import AboutUs from './AboutUs page/AboutUs';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import AboutUs from "./AboutUs page/AboutUs";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../src/Components/Navbar";
 
-import Navbar from '../src/Components/Navbar';
+const Home = () => <h2>Home Component</h2>;
+const Budget = () => <h2>Budget Component</h2>;
+const Expenditures = () => <h2>Expenditures Component</h2>;
+const Transaction = () => <h2>Transaction Component</h2>;
+const Login = () => <h2>Login Component</h2>;
+const Signup = () => <h2>Signup Component</h2>;
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AboutUs/>
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/budget" element={<Budget />} />
+          <Route path="/expenditures" element={<Expenditures />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
-
-    
-    
-
   );
 }
 
 export default App;
-
--- spend-smart/src/Components/Navbar.js
+++ spend-smart/src/Components/Navbar.js
@@ -1,25 +1,14 @@
-
-import React from 'react';
-import { BrowserRouter as Router, NavLink, Routes, Route } from 'react-router-dom';
-import '../CSS/Navbar.css';
-import logo from '../image/logo4.png';
-
-const Home = () => <h2>Home Component</h2>;
-const Budget = () => <h2>Budget Component</h2>;
-const Expenditures = () => <h2>Expenditures Component</h2>;
-const Transaction = () => <h2>Transaction Component</h2>;
-const About = () => <h2>About Component</h2>;
-const Login = () => <h2>Login Component</h2>;
-const Signup = () => <h2>Signup Component</h2>;
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../CSS/Navbar.css";
+import logo from "../image/logo4.png";
 
 const Navbar = () => {
   return (
     <div className="container navbar">
       <nav className="nav-0">
         <div className="nav-contents">
-          <div className="nav-title">
-            SPENDMART
-          </div>
+          <div className="nav-title">SPENDMART</div>
           <div>
             <img src={logo} alt="Logo" className="logo" />
           </div>
@@ -28,19 +17,29 @@ const Navbar = () => {
         <div className="content-links">
           <ul>
             <li>
-              <NavLink exact to="/" activeClassName="active">Home</NavLink>
+              <NavLink exact to="/" activeClassName="active">
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/budget" activeClassName="active">Budget</NavLink>
+              <NavLink to="/budget" activeClassName="active">
+                Budget
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/expenditures" activeClassName="active">Expenditures</NavLink>
+              <NavLink to="/expenditures" activeClassName="active">
+                Expenditures
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/transaction" activeClassName="active">Transaction</NavLink>
+              <NavLink to="/transaction" activeClassName="active">
+                Transaction
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="active">About</NavLink>
+              <NavLink to="/about" activeClassName="active">
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -48,10 +47,14 @@ const Navbar = () => {
         <div className="content-links">
           <ul>
             <li>
-              <NavLink to="/login" activeClassName="active">Login</NavLink>
+              <NavLink to="/login" activeClassName="active">
+                Login
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/signup" activeClassName="active">Sign Up</NavLink>
+              <NavLink to="/signup" activeClassName="active">
+                Sign Up
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -60,29 +63,4 @@ const Navbar = () => {
   );
 };
 
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/budget" element={<Budget />} />
-        <Route path="/expenditures" element={<Expenditures />} />
-        <Route path="/transaction" element={<Transaction />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
-
-
-
-
-
-
-
-
+export default Navbar;
