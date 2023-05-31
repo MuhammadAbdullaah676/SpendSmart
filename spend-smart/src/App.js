7346c70d28763166746fb2a5b1c7df1d4bc3ef9e
amensheikh <134705651+amensheikh@users.noreply.github.com>
2023-06-01 04:10:36 +0500

    Add files via upload
    complete frontend home page ready

--- spend-smart/src/App.js
+++ spend-smart/src/App.js
@@ -1,31 +1,16 @@
-import "./App.css";
-import AboutUs from "./AboutUs page/AboutUs";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Components/Login";
-import Register from "./Components/Register";
-import Navbar from "../src/Components/Navbar";
-
-const Home = () => <h2>Home Component</h2>;
-const Budget = () => <h2>Budget Component</h2>;
-const Expenditures = () => <h2>Expenditures Component</h2>;
-const Transaction = () => <h2>Transaction Component</h2>;
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import SignInPage from './pages/signin';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/budget" element={<Budget />} />
-          <Route path="/expenditures" element={<Expenditures />} />
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<SignInPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
