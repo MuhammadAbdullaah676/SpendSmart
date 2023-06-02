ffbee453c378940ae29e3aedb1d79f226970b1a7
batool <tahiarb777@gmail.com>
2023-06-02 16:18:33 +0500

    expense changes

--- spend-smart/src/App.js
+++ spend-smart/src/App.js
@@ -1,44 +1,30 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Budget from './components/Budget';
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import AddExpenseForm from './components/AddExpenseForm';
+import React from "react";
+//import Formik1 from "./component/Formik";
+//import PostList from "./component/card/Card";
+//import UserList from "./component/card/Table";
 
 
-const App = () => {
-	return (
-		
-			<div className='container'>
-				<h1 className='mt-3'>Spend Smart</h1>
-				<div className='row mt-3'>
-					{<div className='col-sm'>
-						<Budget />
-					</div> }
-					<div className='col-sm'>
-						<Remaining />
-					</div>
-					<div className='col-sm'>
-						<ExpenseTotal />
-					</div>
-				</div>
-				<h3 className='mt-3'>Expenses</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<ExpenseList />
-					</div>
-				</div>
-				<h3 className='mt-3'>Add Expense</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<AddExpenseForm />
-					</div>
-				</div>
-			</div>
-		
-	);
-};
+
+import {Header} from './components/Header'
+import{Balance} from './components/Balance'
+import {IncomeExpenses} from './components/IncomeExpenses'
+import {TransactionList} from './components/TransactionList'
+import{AddTransaction} from './components/AddTransaction'
+import{GlobalProvider} from './components/context/GlobalState'
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+    <Header/>
+    <div className="container">
+     <Balance/>
+     <IncomeExpenses/>
+     <TransactionList/>
+     <AddTransaction/>
+    </div>
+    </GlobalProvider>
+  );
+}
 
 export default App;
-
